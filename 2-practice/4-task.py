6b1681a8905630753b5a39dd954ca9406be87c30
Ilya Semakin <il.semakin@mail.ru>
2021-11-22 14:07:30 +0300

    Calculating the area of a circle by radius

---
+++ 2-practice/4-task.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import math
+
+try:
+    radius = float(input('Input the radius: '))
+except ValueError:
+    print('Error: You input not a number!')
+else:
+    print(f'Area of circle = { math.pi * math.pow(radius, 2) }')

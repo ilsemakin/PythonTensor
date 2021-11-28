2d4a19bcc46378e7e10880424f7926fe991582b1
Ilya Semakin <il.semakin@mail.ru>
2021-11-28 18:18:35 +0300

    Removed infinite repetition

--- 3-practice/1-task.py
+++ 3-practice/1-task.py
@@ -5,25 +5,23 @@ up = ['up', 'вверх']
 down = ['down', 'вниз']
 right = ['right', 'вправо']
 left = ['left', 'влево']
-stop = ['stop', 'стоп']
 
 def main():
     x = y = 0
 
-    while True:
-        command = input_command('Enter the command: ')
+    command = input_command('Enter the command: ')
 
-        movement = command[0]
-        step = command[1]
+    movement = command[0]
+    step = command[1]
 
-        _x, _y = x, y
+    _x, _y = x, y
 
-        if movement in up: y += step
-        elif movement in down: y -= step
-        elif movement in right: x += step
-        elif movement in left: x -= step
+    if movement in up: y += step
+    elif movement in down: y -= step
+    elif movement in right: x += step
+    elif movement in left: x -= step
 
-        print(f'({_x}; {_y}) -> ({x}; {y})\n')
+    print(f'({_x}; {_y}) -> ({x}; {y})\n')
 
 def input_command(message):
     movements = up + down + right + left
@@ -33,14 +31,11 @@ def input_command(message):
             command = input(message).split(' ')
             movement = command[0]
 
-            if movement in stop: sys.exit()
-            
             if movement not in movements or len(command) != 2:
                 print('Command not defined!\n')
             else: 
                 command[1] = float(command[1])
                 return command
-
         except ValueError:
             print('The input is not a number!\n')
         except KeyboardInterrupt:

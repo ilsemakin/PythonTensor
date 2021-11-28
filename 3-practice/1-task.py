44652f51c251fea765f600930874e6ba7ec2d1e8
Ilya Semakin <il.semakin@mail.ru>
2021-11-28 17:50:46 +0300

    Movement along coordinate axes

---
+++ 3-practice/1-task.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+import sys
+
+up = ['up', 'вверх']
+down = ['down', 'вниз']
+right = ['right', 'вправо']
+left = ['left', 'влево']
+stop = ['stop', 'стоп']
+
+def main():
+    x = y = 0
+
+    while True:
+        command = input_command('Enter the command: ')
+
+        movement = command[0]
+        step = command[1]
+
+        _x, _y = x, y
+
+        if movement in up: y += step
+        elif movement in down: y -= step
+        elif movement in right: x += step
+        elif movement in left: x -= step
+
+        print(f'({_x}; {_y}) -> ({x}; {y})\n')
+
+def input_command(message):
+    movements = up + down + right + left
+
+    while True:
+        try:
+            command = input(message).split(' ')
+            movement = command[0]
+
+            if movement in stop: sys.exit()
+            
+            if movement not in movements or len(command) != 2:
+                print('Command not defined!\n')
+            else: 
+                command[1] = float(command[1])
+                return command
+
+        except ValueError:
+            print('The input is not a number!\n')
+        except KeyboardInterrupt:
+            sys.exit('')
+     
+main()

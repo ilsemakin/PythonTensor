713f98b975e63a66e180c4bf1a7b199bdda6aca6
Ilya Semakin <il.semakin@mail.ru>
2021-12-01 03:17:43 +0300

    Classic operations using sets

---
+++ 4-practice/3-task.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import random
+
+set_1 = set(random.randint(-50, 50) for _ in range(20))
+set_2 = set(random.randint(-50, 50) for _ in range(20))
+
+print(f'set1 = {set_1}\nset2 = {set_2}\n')
+
+print(f'Intersection = {set_1 & set_2}')
+print(f'Difference set1 and set2 = {set_1 - set_2}')
+print(f'Difference set2 and set1 = {set_2 - set_1}')
+print(f'Symmetric difference = {set_1 ^ set_2}')

b1595b69c3629bfdba9f11a9ca8b58cd002b4e30
Ilya Semakin <il.semakin@mail.ru>
2021-12-08 12:24:27 +0300

    Encoding and decoding a list of strings

---
+++ 6-practice/1-task.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def main():
+    encoded_strings = encode_list(input_list('Enter a list of strings: '))
+    decoded_strings = decode_list(encoded_strings)
+
+    print(f'Encoded strings: {encoded_strings}')
+    print(f'Decoded strings: {decoded_strings}')
+
+def encode_list(strings): return [tuple(ord(symbol) for symbol in string) for string in strings]
+def decode_list(strings): return [''.join([chr(code) for code in string]) for string in strings]
+def input_list(message): return [string for string in input(message).split(' ') if string]
+
+main()
